b8ec21d67ffb25967d196b2759a4fce8f53a0c20
Radheya-2811 <dubeyh1414@gmail.com>
2024-06-27 19:40:05 +0530

    EventJS

--- Events(JS)/script.js
+++ Events(JS)/script.js
@@ -1,18 +1,18 @@
-// document.getElementById('prayer').onclick=function () {
-//     alert("prayer selected")
-// }
+document.getElementById('prayer').onclick=function () {
+    alert("prayer selected")
+}
 
-// document.getElementById("prayer").addEventListener('click',function(e){
-//     console.log(e);
-// })
-// document.getElementById('prayer').addEventListener('click',function(e){
-//     console.log("prayer clicked");
+document.getElementById("prayer").addEventListener('click',function(e){
+    console.log(e);
+})
+document.getElementById('prayer').addEventListener('click',function(e){
+    console.log("prayer clicked");
     
-// },true)
-// document.getElementById("images").addEventListener('click',function(e){
-//     console.log("images clicked");
-//     e.stopPropagation();
-// },true)
+},true)
+document.getElementById("images").addEventListener('click',function(e){
+    console.log("images clicked");
+    e.stopPropagation();
+},true)
 
 const images=document.querySelector('#images')
 images.addEventListener('click',function(e){
